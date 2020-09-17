a5320ff5cc5d557ab3b1b02955387d74a6f6a2d7
ThucNguyen <macbook@MacBooks-MacBook-Pro-2.local>
2020-09-17 21:46:19 +0700

    fix lỗi thư viện ảnh, android

--- android/app/src/main/java/com/vieclam/phothong/MainApplication.java
+++ android/app/src/main/java/com/vieclam/phothong/MainApplication.java
@@ -1,5 +1,6 @@
 package com.vieclam.phothong;
 
+import com.imagepicker.ImagePickerPackage;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
@@ -26,7 +27,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          new ImagePickerPackage();
           return packages;
         }
 
